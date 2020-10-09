e12206e8b923820f706014617c7f93642261e26b
Kalia-Lee Rodney <kaliarodney@gmail.com>
2020-10-08 20:06:22 -0500

    Squares added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,13 @@
 "use strict";
 
-document.body.onload = function addClassName(){
-    let elem = document.getElementsByTagName("div").childNodes;
 
-    for (i=0; i < elem.length ; i++){
-        elem.className += "square";
+window.onload = function addClassName() {
+    let elem = document.querySelectorAll("#board div");
+
+    for (let i=0; i < elem.length ; i++){
+        elem[i].className += "square";
     }
-    
 }
-
 
     
        
